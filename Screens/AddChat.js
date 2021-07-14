253b0edb279f539a16aed2368514c483b969f00c
FARKADI OUSSAMA <58217000+Farkadi06@users.noreply.github.com>
2021-07-14 10:48:25 +0100

    Creating the addChat screen UI

--- Screens/AddChat.js
+++ Screens/AddChat.js
@@ -1,14 +1,43 @@
 import React from 'react'
+import { useState } from 'react'
+import { useLayoutEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import {Button, Input} from "react-native-elements"
+import { Icon } from 'react-native-vector-icons/FontAwesome'
+import { db } from '../firebase'
 
-const AddChat = () => {
+const AddChat = ({navigation}) => {
+    const [input, setInput]= useState("");
+
+    useLayoutEffect(()=>{
+        navigation.setOptions({
+            title:"Add a new Chat",
+            headerBackTitle: "Chats"
+        });
+    },[navigation]);
+
+
+    const createChat = async()=> {
+        
+    }
+
     return (
-        <View>
-            <Text></Text>
+        <View styles={styles.container}>
+            <Input 
+                placeholder="Enter a chat name"
+                value={input}
+                onChange={(text) => setInput(text)}
+                onSubmitEditing={createChat}
+                />
+            <Button onPress={createChat} title="Create new Chat" />
         </View>
     )
 }
 
 export default AddChat
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container :  {
+
+    }
+})
