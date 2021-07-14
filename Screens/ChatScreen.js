ffcbc2facadc959bf54f4347f305006c2bfcfde9
FARKADI OUSSAMA <58217000+Farkadi06@users.noreply.github.com>
2021-07-14 12:53:46 +0100

    finishing the ChatScreen Header and creating the input and button footer

--- Screens/ChatScreen.js
+++ Screens/ChatScreen.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useLayoutEffect } from 'react'
 import { TouchableOpacity } from 'react-native'
 import { StyleSheet, Text, View } from 'react-native'
 import { Avatar } from 'react-native-elements/dist/avatar/Avatar'
+import {AntDesign, SimpleLineIcons, Ionicons} from '@expo/vector-icons';
+import { SafeAreaView } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import { KeyboardAvoidingView } from 'react-native'
+import { Platform } from 'react-native'
+import { ScrollView } from 'react-native'
+import { TextInput } from 'react-native'
+import { color } from 'react-native-elements/dist/helpers'
 
 const ChatScreen = ({navigation, route}) => {
+    const [input,setInput] = useState('')
     useLayoutEffect(()=>{
         navigation.setOptions({
             title: "Chat",
@@ -28,16 +37,81 @@ const ChatScreen = ({navigation, route}) => {
                     onPress={navigation.goBack}>
                     <AntDesign name="arrowleft" size= {24} color="white"/>
                 </TouchableOpacity>
+            ),
+            headerRight: ()=> (
+                <View style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    width: 80,
+                    marginRight: 20,
+                }}>
+                    <TouchableOpacity>
+                      <AntDesign name="videocamera" size= {24} color="white"/>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                      <AntDesign name="phone" size= {24} color="white"/>
+                    </TouchableOpacity>
+                </View>
             )
         })
     },[navigation])
+
+
+    const sendMessage = ()=>{
+
+    }
     return (
-        <View>
-            <Text>{route.params.chatName}</Text>
-        </View>
+        <SafeAreaView style={{flex:1, backgroundColor: "white"}}>
+            <StatusBar style="light" />
+            <KeyboardAvoidingView
+                behavior={Platform.OS === "ios" ? "padding": "height"}
+                style={styles.container}
+                keyboardVerticalOffset = { 100}
+                >
+                    <>
+                        <ScrollView>
+                             
+                        </ScrollView>
+                        <View style={styles.footer}>
+                            <TextInput
+                                placeholder='Signal Message'
+                                style={styles.textInput}
+                                value={input}
+                                onChangeText={(text) => setInput(text)}
+                                    />
+                            <TouchableOpacity 
+                                activeOpacity={0.5} 
+                                onPress={sendMessage}
+                            >
+                                <Ionicons name="send" size={24} color="#2B68E6"/>
+                            </TouchableOpacity>
+                        </View>
+                    </>
+            </KeyboardAvoidingView>
+        </SafeAreaView>
     )
 }
 
 export default ChatScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+    },
+    footer: {
+        flexDirection: "row",
+        alignItems: "center",
+        width: "100%",
+        padding: 15,
+    },
+    textInput: {
+        bottom: 0,
+        height: 40,
+        flex: 1,
+        marginRight: 15,
+        backgroundColor: "#ECECEC", 
+        padding: 10,
+        color: "grey",
+        borderRadius: 30
+    }
+})
