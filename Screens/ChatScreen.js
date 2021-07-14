910271178f9f1ff3d7c71dc84a3a56a4e8f83be2
FARKADI OUSSAMA <58217000+Farkadi06@users.noreply.github.com>
2021-07-14 14:04:50 +0100

    Finalise The app

--- Screens/ChatScreen.js
+++ Screens/ChatScreen.js
@@ -11,9 +11,13 @@ import { Platform } from 'react-native'
 import { ScrollView } from 'react-native'
 import { TextInput } from 'react-native'
 import { color } from 'react-native-elements/dist/helpers'
+import { Keyboard } from 'react-native'
+import { TouchableWithoutFeedback } from 'react-native'
+import { db,auth, firebase } from '../firebase'
 
 const ChatScreen = ({navigation, route}) => {
     const [input,setInput] = useState('')
+    const [messages,setMessages] = useState([])
     useLayoutEffect(()=>{
         navigation.setOptions({
             title: "Chat",
@@ -26,7 +30,7 @@ const ChatScreen = ({navigation, route}) => {
                 }}>
                     <Avatar  rounded 
                              source={{
-                             uri : "https://business-on-line.fr/wp-content/uploads/2016/10/6a0120a8b67743970b01b7c7ca52af970b-500wi.jpg" }}
+                             uri : messages[0]?.data.photoURL || "https://business-on-line.fr/wp-content/uploads/2016/10/6a0120a8b67743970b01b7c7ca52af970b-500wi.jpg"}}
                              />
                     <Text style={{color: "white", marginLeft: 10, fontWeight: "700"}}>{route.params.chatName }</Text>
                 </View>
@@ -54,12 +58,33 @@ const ChatScreen = ({navigation, route}) => {
                 </View>
             )
         })
-    },[navigation])
+    },[navigation, messages])
 
 
     const sendMessage = ()=>{
+        Keyboard.dismiss();
+        db.collection('chats').doc(route.params.id).collection('messages').add({
+          timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+          message: input,
+          displayName: auth.currentUser.displayName,
+          email: auth.currentUser.email,
+          photoURL: auth.currentUser.photoURL
+        })
 
+        setInput("")
     }
+
+    useLayoutEffect(()=> {
+        const unsubscribe = db.collection('chats').doc(route.params.id).collection('messages').orderBy('timestamp', 'desc').onSnapshot(
+            snapShot => setMessages(
+                snapShot.docs.map(doc => ({
+                    id: doc.id,
+                    data: doc.data()
+                }))
+            )
+        );
+        return unsubscribe;
+    },[route])
     return (
         <SafeAreaView style={{flex:1, backgroundColor: "white"}}>
             <StatusBar style="light" />
@@ -68,25 +93,67 @@ const ChatScreen = ({navigation, route}) => {
                 style={styles.container}
                 keyboardVerticalOffset = { 100}
                 >
-                    <>
-                        <ScrollView>
-                             
-                        </ScrollView>
-                        <View style={styles.footer}>
-                            <TextInput
-                                placeholder='Signal Message'
-                                style={styles.textInput}
-                                value={input}
-                                onChangeText={(text) => setInput(text)}
-                                    />
-                            <TouchableOpacity 
-                                activeOpacity={0.5} 
-                                onPress={sendMessage}
-                            >
-                                <Ionicons name="send" size={24} color="#2B68E6"/>
-                            </TouchableOpacity>
-                        </View>
-                    </>
+                    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                        <>
+                            <ScrollView contentContainerStyle={{paddingTop:15}}>
+                                {
+                                    messages.map(({id, data}) =>(
+                                        data.email === auth.currentUser.email ? (
+                                                <View style={styles.reciever}>
+                                                    <Avatar 
+                                                    containerStyle={{
+                                                        position: "absolute",
+                                                        bottom:-15,
+                                                        right:-5
+                                                    }}
+                                                    rounded
+                                                    size={30}
+                                                    bottom={-15}
+                                                    right={-5}
+                                                        source={{
+                                                            uri: data?.photoURL || "https://business-on-line.fr/wp-content/uploads/2016/10/6a0120a8b67743970b01b7c7ca52af970b-500wi.jpg",
+                                                        }}/>
+                                                    <Text style={styles.recieverText} > {data.message}</Text>
+                                                </View>
+                                        ):(
+                                                <View style={styles.sender}>
+                                                    <Avatar 
+                                                    containerStyle={{
+                                                        position: "absolute",
+                                                        bottom:-15,
+                                                        right:-5
+                                                    }}
+                                                    rounded
+                                                    size={30}
+                                                    bottom={-15}
+                                                    right={-5}
+                                                        source={{
+                                                            uri: data?.photoURL || "https://business-on-line.fr/wp-content/uploads/2016/10/6a0120a8b67743970b01b7c7ca52af970b-500wi.jpg",
+                                                        }}/>
+                                                    <Text style={styles.senderText} > {data.message}</Text>
+                                                    <Text style={styles.senderName} > {data.diplayName}</Text>
+                                                </View>
+                                        )
+                                    ))
+                                }
+                            </ScrollView>
+                            <View style={styles.footer}>
+                                <TextInput
+                                    placeholder='Signal Message'
+                                    style={styles.textInput}
+                                    value={input}
+                                    onChangeText={(text) => setInput(text)}
+                                    onSubmitEditing={sendMessage}
+                                        />
+                                <TouchableOpacity 
+                                    activeOpacity={0.5} 
+                                    onPress={sendMessage}
+                                >
+                                    <Ionicons name="send" size={24} color="#2B68E6"/>
+                                </TouchableOpacity>
+                            </View>
+                        </>
+                    </TouchableWithoutFeedback>
             </KeyboardAvoidingView>
         </SafeAreaView>
     )
@@ -113,5 +180,43 @@ const styles = StyleSheet.create({
         padding: 10,
         color: "grey",
         borderRadius: 30
+    },
+    reciever: {
+        padding: 15,
+        backgroundColor: "#ECECEC",
+        alignSelf: "flex-end",
+        borderRadius: 20,
+        marginRight: 15,
+        marginBottom: 20,
+        maxWidth: "80%",
+        position: "relative"
+
+    },
+    sender: {
+        padding: 15,
+        backgroundColor: "#2C68ED",
+        alignSelf: "flex-start",
+        borderRadius: 20,
+        marginRight: 15,
+        marginBottom: 20,
+        maxWidth: "80%",
+        position: "relative"
+    },
+    recieverText: {
+        color: "black",
+        fontWeight: "500",
+        marginLeft: 10,
+    },
+    senderText:{
+        color: "white",
+        fontWeight: "500",
+        marginLeft: 10,
+        marginBottom: 15
+    },
+    senderName: {
+        left: 10,
+        paddingRight: 10,
+        fontSize: 10,
+        color: "white"
     }
 })
--- components/CustomListItem.js
+++ components/CustomListItem.js
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native'
 import { ListItem, Avatar } from 'react-native-elements';
+import { db } from '../firebase';
 
 const CustomListItem = ({id, chatName, enterChat}) => {
+    const [chatMessages, setChatMessages] = useState()
+
+    useEffect(()=>{
+        const unsubscribe = db.collection('chats').doc(id).collection('messages').orderBy('timestamp', 'desc').onSnapshot(
+            snapshot => {
+                setChatMessages(snapshot.docs.map(doc => doc.data()))
+            }
+        );
+        return unsubscribe;
+    },[])
+
     return (
-        <ListItem onPress={()=> enterChat(id, chatName)} key={id} bottomDivider>
+        <ListItem key={id} onPress={()=> enterChat(id, chatName)} key={id} bottomDivider>
             <Avatar 
                 rounded 
                 source={{
-                    uri:  "https://business-on-line.fr/wp-content/uploads/2016/10/6a0120a8b67743970b01b7c7ca52af970b-500wi.jpg"
+                    uri:  chatMessages?.[0]?.photoURL || "https://business-on-line.fr/wp-content/uploads/2016/10/6a0120a8b67743970b01b7c7ca52af970b-500wi.jpg",
                 }}/>
             <ListItem.Content>
                 <ListItem.Title style={{fontWeight: "800"}}>
                     {chatName}
                 </ListItem.Title>
                 <ListItem.Subtitle numberOfLines={1} ellipsizeMode="tail">
-                    Yoooooooo
+                    {chatMessages?.[0]?.displayName}: {chatMessages?.[0]?.message}
                 </ListItem.Subtitle>
             </ListItem.Content>
         </ListItem>
